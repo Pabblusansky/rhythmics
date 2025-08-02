4e1c5bd1a2c8747eb58d24887dfcc5f05d1634a2
Pavel <pchirtoca@gmail.com>
2025-08-02 14:26:01 +0300

    fix(deploy): configure STATIC_ROOT for deployment

--- backend/rhythmics_project/settings.py
+++ backend/rhythmics_project/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
